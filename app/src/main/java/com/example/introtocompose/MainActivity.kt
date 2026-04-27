0fbfcfb0048d1ecf33bdc7f0caa9c19b660e5ece
Chris <cr1s@hotmail.it>
2026-04-27 16:21:50 +0200

    Add circleShape for tap

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.introtocompose
 
+import android.R
+import android.R.color.white
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +33,55 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IntroToComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(){
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        containerColor = Color(0xFF546E7A)
+        //topBar = { AppBar }
+        //bottomBar = { BottomBar }
+        //floatingActionButton = { FAB }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ){
+            CreateCircle()
+        }
+    }
 }
+
+@Preview
+@Composable
+fun CreateCircle(){
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(145.dp)
+        .clickable {
+            Log.d("TAP", "CreateCircle: Tap")
+        },
+        shape = CircleShape
+    ){
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Text("TAP", modifier = Modifier)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IntroToComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
